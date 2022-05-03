53b0111ab14092b7b1bb17839ba8eb320ee4f3a9
DmitriyRoss <seer6990@gmail.com>
2022-05-03 17:13:30 +0300

    added a few styles and initialized a few components

---
+++ index.js
@@ -0,0 +1,10 @@
+const AddButton = document.getElementById('add-btn')
+const AddInput = document.getElementById('input')
+const InProgress = document.getElementById('inprogress')
+const Done = document.getElementById('done')
+
+function AddNewInput() {
+  let element = document.createElement('input')
+  element.id = input
+  console.log('element created')
+}
